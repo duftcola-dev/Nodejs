af641ca3c169b764ae70d84867117802928c8e63
duftcola-dev <robinviera@hotmail.com>
2022-11-30 00:14:20 +0100

    adding additional CICD support

--- scripts/CICD.sh
+++ scripts/CICD.sh
@@ -1,6 +1,29 @@
 
 Blue='\033[0;34m'         # Blue
-readonly image=nodeservice/backend
+readonly image=duftcola/node_service_template
+
+
+# Login in docker hub
+# Requires the user and password to be set as env variables
+if [ "$1" = "login" ];then
+    echo "Login in your docker hub repository"
+    docker login --username $DOCKER_USER --password $DOCKER_PASSWORD
+    docker push duftcola/node_service_template:latest
+fi
+
+# Requires login
+# Requires the image to be build
+if [ "$1" = "push" ];then
+    echo "Pushing your docker images"
+    docker push duftcola/node_service_template:latest
+fi
+
+# Requires login
+# Requires the image to be build
+if [ "$1" = "pull" ];then
+    echo "Pulling your docker image"
+    docker pull duftcola/node_service_template
+fi
 
 # Build docker image for development/test
 if [ "$1" = "build" ];then
@@ -16,6 +39,7 @@ fi
 if [ "$1" = "docker" ];then
     docker run -td -p 0.0.0.0:8000:8000 -e CI=true -e NODE_ENV=dev --name node_service $image
 fi
+
 # Run docker compose
 if [ "$1" = "compose" ];then
     mv ./docker/docker-compose.yaml ./docker-compose.yaml
